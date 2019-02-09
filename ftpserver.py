6a51cde1e7d805e83171f8ea49fb778131a9c1ff
bgolamco <bgolamco@sfu.ca>
2019-02-09 09:29:31 -0800

    Commit 1

---
+++ ftpserver.py
@@ -0,0 +1,41 @@
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+from pyftpdlib.authorizers import DummyAuthorizer
+import keys as keys
+import ftplib
+
+class serverHandler(FTPHandler):
+
+    def initHandler(self,key,IV):
+        self.key = key
+        self.IV = IV
+
+    def on_file_received(self,file):
+        print("Type of file is: {}".format(type(file)))
+        print(file)
+
+def ftpserver():
+    authorizer = DummyAuthorizer()
+    for i in range(0,len(keys.USER)):
+        authorizer.add_user(keys.USER[i],keys.PASSWORD[i],keys.SERVER_DIR[i],perm=keys.PERMISSIONS[i])
+
+    handler = serverHandler
+    handler.authorizer = authorizer
+
+    server = FTPServer((keys.IP_ADDRESS_SERVER,keys.PORT),handler)
+    server.serve_forever()
+
+def ftpUpload(fileName):
+    ftp = ftplib.FTP('')
+    ftp.connect(keys.IP_ADDRESS_CLIENT,keys.PORT)
+    ftp.login(keys.USER[0],keys.PASSWORD[0])
+    ftp.storbinary('STOR '+fileName,open(fileName,'rb'))
+    ftp.quit()
+    return
+
+def ftpDownload(fileName):
+    ftp = ftplib.FTP('')
+    ftp.connect(keys.IP_ADDRESS_CLIENT,keys.PORT)
+    ftp.login(keys.USER[1],keys.PASSWORD[1])
+    ftp.retrbinary('RETR ' + fileName, open(fileName,'wb').write)
+    ftp.quit()
---
+++ keys.py
@@ -0,0 +1,8 @@
+USER=['uploadAdmin','downloadAdmin']
+PASSWORD=['ABCDEF','ABCDEF']
+PERMISSIONS=['w','r']
+SERVER_DIR = ['./receiver','./receiver']
+IP_ADDRESS_CLIENT='localhost'
+IP_ADDRESS_SERVER='0.0.0.0'
+CONN_PASSWORD = "asbasd"
+PORT=5002
