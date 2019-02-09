29e77a4c33f4c6ceaba97f68c9f084d77c05ddb4
cyrilderamos <cyril_97@hotmail.com>
2019-02-09 15:36:10 -0800

    modified utils.py added local_delete added local_undo_delete

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import hashlib
+import shutil
 
 class save_methods():
 	def __init__(self):
@@ -49,3 +50,39 @@ def write_checksum(write, checksum_name):
 def read_checksum(filename):
 	lines = [line.rstrip('\n') for line in open(filename)]
 	return lines
+
+def local_delete(fileName):
+    old_file_path = os.getcwd() + '/' + fileName
+
+    if os.path.isdir(os.getcwd() + '/' + 'tempDir') == 1:
+        if len(os.listdir(os.getcwd() + '/' + 'tempDir')) == 0:
+            new_file_path = os.getcwd() + '/' + 'tempDir' + '/' + fileName
+            os.rename(old_file_path, new_file_path)
+        else:
+            shutil.rmtree(os.getcwd() + '/' + 'tempDir')
+            temp_file_dir = os.getcwd() + '/' + 'tempDir'
+            os.mkdir(temp_file_dir)
+
+            new_file_path = os.getcwd() + '/' + 'tempDir' + '/' + fileName
+            os.rename(old_file_path, new_file_path)
+    else:
+        temp_file_dir = os.getcwd() + '/' + 'tempDir'
+        os.mkdir(temp_file_dir)
+
+        new_file_path = temp_file_dir + '/' + fileName
+        os.rename(old_file_path, new_file_path)
+
+def local_undo_delete(fileName):
+    temp_Dir = os.getcwd() + '/' + 'tempDir'
+    try:
+        if os.path.isdir(temp_Dir) == 1:
+            if len(temp_Dir + '/' + 'tempDir') == 0:
+                raise
+            else:
+                old_file_path = os.getcwd() + '/' + 'tempDir' + '/' + fileName
+                new_file_path = os.getcwd() + '/' + fileName
+                os.rename(old_file_path, new_file_path)
+        else:
+            raise
+    except:
+        raise Exception("Error: Can't undo delete")
